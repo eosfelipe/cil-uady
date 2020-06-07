70ae55f94d83b7d9970af3fe70988252021e4147
eosfelipe <escobedo.felipe@hotmail.com>
2020-06-06 20:16:45 -0500

    add colors, fontawesome, logo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 require('./css/styles.scss');
+require('./fa/all.min.js');
+const logo = require('./img/logo.png');
+
+const img = document.querySelector('#logo');
+img.src = logo.default;
--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true,
-                        //options
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            //options
+                        }
                     }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'img',
+                    publicPath: 'img',
+                    name: '[sha512:hash:base64:7].[ext]'
                 }
-            ]
-        }]
+            },
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
